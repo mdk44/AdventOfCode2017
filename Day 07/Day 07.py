819c6d2a38245dc093fc25eef18fe46a33577ddc
mdk44 <matt.karam@gmail.com>
2020-02-18 20:38:15 -0600

    Tinkering with Day 7

--- Day 07/Day 07.py
+++ Day 07/Day 07.py
@@ -1,23 +1,32 @@
 import re
 
-input_file = 'Day 07\\Input.csv'
-# input_file = 'Day 07\\Test.csv'
+# input_file = 'Day 07\\Input.csv'
+input_file = 'Day 07\\Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-def return_bottom(lines):
-    branches = []
-    tree = []
-    for line in lines:
-        tree.append(line.split(' ')[0])
-        if '->' in line:
-            new = line.split(' -> ')[1]
-            branch = new.split(', ')
-            for b in branch:
-                branches.append(b)
-    for t in tree:
-        if t not in branches:
-            return t
+weight = dict()
+branches = dict()
+for line in lines:
+    numbers = re.findall(r"\d+", line)
+    tree = line.split(' ')[0]
+    weight[tree] = int(numbers[0])
+    if '->' in line:
+        new = line.split(' -> ')[1]
+        branch = new.split(', ')
+        branches[tree] = branch
 
+def return_bottom(branches):
+    check = True
+    for b in branches.keys():
+        check = False
+        for v in branches.values():
+            if b in v:
+                check = True
+        if check == False:
+            return b
+                
 
-print("Part 1: " + return_bottom(lines)) # Correct!
+# def return_weight(lines):
+    
+print("Part 1: " + str(return_bottom(branches))) # Correct!
