ed4d2ddea38df5826eda27ad15efd1a5b43e2c85
mdk44 <matt.karam@gmail.com>
2020-02-22 08:33:53 -0600

    Getting closer to day 7

--- Day 07/Day 07.py
+++ Day 07/Day 07.py
@@ -1,5 +1,4 @@
-import re
-import itertools
+import re, itertools, collections
 
 # input_file = 'Day 07\\Input.csv'
 input_file = 'Day 07\\Test.csv'
@@ -8,14 +7,20 @@ lines = text_file.read().split('\n')
 
 weight = dict()
 branches = dict()
+total_weight = dict()
+root = []
 for line in lines:
     numbers = re.findall(r"\d+", line)
     tree = line.split(' ')[0]
     weight[tree] = int(numbers[0])
+    total_weight[tree] = int(numbers[0])
     if '->' in line:
         new = line.split(' -> ')[1]
         branch = new.split(', ')
-        branches[tree] = branch
+        branches[tree] = tuple(branch)
+for b in weight:
+    if b not in branches:
+        root.append(b)
 
 def return_bottom(branches):
     check = True
@@ -27,15 +32,6 @@ def return_bottom(branches):
         if check == False:
             return b
 
-def return_weight():
-    total_weight = dict()
-    for tree in weight:
-        total_weight[tree] = weight[tree]
-        if tree in branches:
-            for branch in branches[tree]:
-                total_weight[tree] += weight[branch]
-    return total_weight
-
 def find_mismatch(tree):
     total_weight = return_weight()
     if tree in branches:
@@ -43,12 +39,18 @@ def find_mismatch(tree):
             if total_weight[a] != total_weight[b]:
                 return branches[tree]
 
-def find_bad_branch(tree):
-    total_weight = return_weight()
-    branch = find_mismatch(tree)
-    for b in branch:
-        print(total_weight[b])
+def return_weight(lst):
+    nxt = []
+    for l in lst:
+        for branch in branches:
+            if l in branches[branch]:
+                if branch not in nxt:
+                    nxt.append(branch)
+                total_weight[branch] += weight[l]
+    return nxt
     
 print("Part 1: " + str(return_bottom(branches))) # Correct!
-print(find_mismatch('tknk'))
-print(find_bad_branch('tknk'))
+
+new = return_weight(root)
+for n in new:
+    print(n, total_weight[n])
